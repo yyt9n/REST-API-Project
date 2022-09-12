e1d73248c70ce85d11e192211e1b5780aee3479a
yuto <yuto.hisamatsu@accenture.com>
2022-09-12 23:33:56 +0900

    Prisma Serviceの実装 各Moduleにインポートまで

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [TodoController],
   providers: [TodoService],
 })
