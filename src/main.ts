277a52516485d0359ac62bc795a6208281f78ff6
yuto <h.yyt9n@gmail.com>
2022-09-17 14:57:18 +0900

    コメント追記

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,6 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(process.env.PORT || 3005); // reactは3000
+  await app.listen(process.env.PORT || 3005); // reactは3000, process.env.PORTはdeploy環境が参照する
 }
 bootstrap();
